b4bfcb289cb77b961fe1d6fa567bd606852045fd
FatherofDharma <fatherofdharma@gmail.com>
2019-10-02 15:53:40 -0700

    finish lesson 72

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -6,14 +6,20 @@ import TitleText from '../components/TitleText';
 
 const GameOverScreen = props => {
     return (
-        <View style={styles.screen}>
-            <TitleText>The Game is Over!</TitleText>
-            <Image source={require('../assets/success.png')}/>
-            <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title='NEW GAME' onPress={props.onRestart}/>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/success.png")}
+            style={styles.image}
+            resizeMode="cover"
+          />
         </View>
-    )
+        <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
+        <BodyText>Number was: {props.userNumber}</BodyText>
+        <Button title="NEW GAME" onPress={props.onRestart} />
+      </View>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -21,6 +27,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
     }
 });
 
