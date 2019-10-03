5e5af2d5aebeaffa632633d16f06fa6bcad1f6be
FatherofDharma <fatherofdharma@gmail.com>
2019-10-03 09:12:03 -0700

    finish lesson 76

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,52 +1,71 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, Image } from "react-native";
 
-import BodyText from '../components/BodyText';
-import TitleText from '../components/TitleText';
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import MainButton from "../components/MainButton";
+import Colors from "../constants/colors";
 
 const GameOverScreen = props => {
-    return (
-      <View style={styles.screen}>
-        <TitleText>The Game is Over!</TitleText>
-        <View style={styles.imageContainer}>
-          <Image
-          fadeDuration={1000}
-            // source={require("../assets/success.png")}
-            //network images using links MUST have a set width and height, because react native doesn't know their size
-            source={{
-              uri:
-                'http://www.susanharpertodd.com/sites/default/files/gallerix/albums/1/14/original/57b0e49316c48b87f2ba7fbf7d1da65ef5519f0e.jpg'
-            }}
-            style={styles.image}
-            resizeMode="cover"
-          />
-        </View>
-        <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
-        <BodyText>Number was: {props.userNumber}</BodyText>
-        <Button title="NEW GAME" onPress={props.onRestart} />
+  return (
+    <View style={styles.screen}>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/success.png")}
+          //network images using links MUST have a set width and height, because react native doesn't know their size
+          // source={{
+          //   uri:
+          //     'http://www.susanharpertodd.com/sites/default/files/gallerix/albums/1/14/original/57b0e49316c48b87f2ba7fbf7d1da65ef5519f0e.jpg'
+          // }}
+          style={styles.image}
+          resizeMode="cover"
+        />
       </View>
-    );
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your Phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
+      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    image: {
-        width: '100%',
-        height: '100%'
-    },
-    imageContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 150,
-        borderWidth: 3,
-        borderColor: 'black',
-        overflow: 'hidden',
-        marginVertical: 30
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold"
+  },
+  resultContainer: {
+    marginHorizontal: 50,
+    marginVertical: 15
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20
+  }
 });
 
 export default GameOverScreen;
