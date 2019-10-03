ba71db03036af1f612540d2a5903362a00d23aa5
FatherofDharma <fatherofdharma@gmail.com>
2019-10-03 14:14:46 -0700

    finish lesson 91

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  Dimensions,
+  ScrollView
+} from "react-native";
 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
@@ -8,30 +16,32 @@ import Colors from "../constants/colors";
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/success.png")}
-          //network images using links MUST have a set width and height, because react native doesn't know their size
-          // source={{
-          //   uri:
-          //     'http://www.susanharpertodd.com/sites/default/files/gallerix/albums/1/14/original/57b0e49316c48b87f2ba7fbf7d1da65ef5519f0e.jpg'
-          // }}
-          style={styles.image}
-          resizeMode="cover"
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/success.png")}
+            //network images using links MUST have a set width and height, because react native doesn't know their size
+            // source={{
+            //   uri:
+            //     'http://www.susanharpertodd.com/sites/default/files/gallerix/albums/1/14/original/57b0e49316c48b87f2ba7fbf7d1da65ef5519f0e.jpg'
+            // }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your Phone needed{" "}
+            <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+            guess the number{" "}
+            <Text style={styles.highlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your Phone needed{" "}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{" "}
-          <Text style={styles.highlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -46,13 +56,13 @@ const styles = StyleSheet.create({
     height: "100%"
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get("window").width * 0.7,
+    height: Dimensions.get("window").width * 0.7,
+    borderRadius: (Dimensions.get("window").width * 0.7) / 2,
     borderWidth: 3,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30
+    marginVertical: Dimensions.get("window").height / 30
   },
   highlight: {
     color: Colors.primary,
@@ -60,11 +70,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 50,
-    marginVertical: 15
+    marginVertical: Dimensions.get("window").height / 60
   },
   resultText: {
     textAlign: "center",
-    fontSize: 20
+    fontSize: Dimensions.get("window").height < 400 ? 16 : 20
   }
 });
 
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Alert,
   ScrollView,
-  FlatList
+  FlatList,
+  Dimensions
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -114,7 +115,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%"
   },
@@ -131,7 +132,7 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 1,
-    width: "60%"
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%"
   },
   list: {
     flexGrow: 1,
