ccc8e661a8c633947776e3d38f4c9308dd58f8fb
FatherofDharma <fatherofdharma@gmail.com>
2019-10-03 12:09:19 -0700

    finish lesson 88.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,7 +6,8 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Dimensions
 } from "react-native";
 
 import Card from "../components/Card";
@@ -130,7 +131,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white"
   },
   button: {
-    width: 100
+    width: Dimensions.get("window").width / 4
   },
   input: {
     width: 50,
